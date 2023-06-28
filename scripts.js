011d46e92461de576151b2f171ab783e07c56605
Danilo-Pereira-of <danilosantosadm95@gmail.com>
2023-06-27 23:55:03 -0300

    add-alert-

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
 
-function generateNumber(){
+function generateNumber() {
 
-    const min = Math.ceil( document.querySelector('.input-min').value)
-    const max = Math.floor( document.querySelector('.input-max').value)
+    const min = Math.ceil(document.querySelector('.input-min').value)
+    const max = Math.floor(document.querySelector('.input-max').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
+    if (min >= max) {
+        alert("Atenção, percebemos que você inverteu os valores, o valor minimo tem que ser menor que o valor máximo.")
+    } else {
+        var resultnumber = Math.floor(Math.random() * (max - min + 1) + min);
+    }
 
-    document.getElementById('result').innerHTML = result
+
+
+    var resultext = "O número sorteado foi:"
+    var resultnumber = Math.floor(Math.random() * (max - min + 1) + min);
+    var resultfinal = resultext + "<br>" + resultnumber;
+
+    document.getElementById('result').innerHTML = resultfinal
 }
